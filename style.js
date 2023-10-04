8606b09205c2cca7afba006869ceefd76e49e17e
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-04 10:21:55 -0300

    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
