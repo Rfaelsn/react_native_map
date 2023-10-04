899c59f083e00ce531297540004434914ee6d949
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-10-04 10:30:55 -0300

    feat: implementando permissao a localização atual do usuario com expo-loccation

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import { View } from 'react-native';
 import { styles } from './style';
+import {
+   requestForegroundPermissionsAsync,
+   getCurrentPositionAsync,
+} from 'expo-location';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+
+  async function requestLocationPermissions(){
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if(granted){
+      const currentPosition = await getCurrentPositionAsync();  
+      setLocation(currentPosition);
+      console.log("Localização: ",currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  },[]);
+
   return (
     <View style={styles.container}>
       
